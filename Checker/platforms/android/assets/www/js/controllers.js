70c9b77d5c4e903ff4ccb687990190d33d39ebe2
Taras <rredcat@gmail.com>
2015-08-26 22:25:33 +0300

    Initial commit

---
+++ Checker/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('CheckCtrl', function ($scope) { })
+
+.controller('ChatsCtrl', function ($scope) {
+    $scope.items = [];
+    //$scope.addItem = function () {
+    //    $scope.items.push({
+    //        name: $scope.itemName
+    //    });
+
+    //    $scope.itemName = "";
+    //};
+    $scope.addItem = function (itemName) {
+        $scope.items.push({name: itemName});
+        $scope.itemName = "";
+    };
+    $scope.removeItem = function (index) {
+        $scope.items.splice(index, 1);
+    };
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
