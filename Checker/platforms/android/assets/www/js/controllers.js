11251322423e9993d2a32b0edab52e33908ab379
unknown <tar@pc-tar.csia.net>
2015-09-10 11:58:55 +0300

    add build file

--- Checker/platforms/android/assets/www/js/controllers.js
+++ Checker/platforms/android/assets/www/js/controllers.js
@@ -1,35 +1,48 @@
 angular.module('starter.controllers', [])
 
-.controller('CheckCtrl', function ($scope) { })
+.controller('CheckCtrl', function ($scope, Chats) {
+    $scope.tasks = Chats.all();
+})
+.controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.allWithNew();
+})
+.controller('ChatDetailCtrl', function ($scope, $state, $stateParams, Chats) {
+    var id = $stateParams.chatId;
+    $scope.isNew = "0" == id;
+    $scope.chat = Chats.get(id);
+    $scope.i21n = $scope.isNew
+    ? { remove: "Cansel", save: "Create" }
+    : { remove: "Remove", save: "Save" };
 
-.controller('ChatsCtrl', function ($scope) {
-    $scope.items = [];
-    $scope.newItem = { name: "", monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
-
-    $scope.addItem = function () {
-        var name = $scope.newItem.name;
-
-        if (typeof name === 'undefined' || '' == name) return;
-
-        $scope.items.push($scope.newItem);
-        $scope.newItem = { name: "", monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
-
-        //var initWeek = function () {//Make cycle in view. But later..
-        //    return {monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
-        //};
-        // Add validation checker!! But later..
+    $scope.remove = function (chat) {
+        //Chats.remove(chat);
+        $state.go('tab.chats');
     };
-    $scope.removeItem = function (index) {
-        $scope.items.splice(index, 1);
+    $scope.save = function (chat) {
+        $state.go('tab.chats');
     };
 })
-
-.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
-})
-
 .controller('AccountCtrl', function ($scope) {
     $scope.settings = {
         enableFriends: true
     };
 });
+//$scope.items = [];
+//$scope.newItem = { name: "", monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
+
+//$scope.addItem = function () {
+//    var name = $scope.newItem.name;
+
+//    if (typeof name === 'undefined' || '' == name) return;
+
+//    $scope.items.push($scope.newItem);
+//    $scope.newItem = { name: "", monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
+
+//    //var initWeek = function () {//Make cycle in view. But later..
+//    //    return {monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false };
+//    //};
+//    // Add validation checker!! But later..
+//};
+//$scope.removeItem = function (index) {
+//    $scope.items.splice(index, 1);
+//};
