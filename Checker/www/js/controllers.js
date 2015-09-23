79d6ee37eb946204ad75fe916f81c9a71be0acd4
Taras <rredcat@gmail.com>
2015-09-23 21:40:09 +0300

    Revert "cansel logic is improved. but still something is wrong"
    This reverts commit d9af47020191bdba1176a0e517bb54ebee9a1997.

--- Checker/www/js/controllers.js
+++ Checker/www/js/controllers.js
@@ -16,11 +16,11 @@ angular.module('starter.controllers', [])
     $scope.chat = Chats.get(id);
     $scope.i21n = $scope.isNew ? { save: "Create" } : { save: "Save" };
 
-    $scope.cancel = function (chat) {
-        $state.go('tab.chats');
-    };
-    $scope.delete = function (chat) {
-        Chats.remove(chat);
+    $scope.remove = function (chat) {
+        if (!$scope.isNew) {
+            Chats.remove(chat);
+        }
+
         $state.go('tab.chats');
     };
     $scope.save = function (chat) {
